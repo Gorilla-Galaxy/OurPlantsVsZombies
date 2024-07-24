3cd275bfaf5226f01724a709ca8d03ef925c7717
Iris-CR <iriscneto@gmail.com>
2024-07-24 10:54:27 -0300

    Revert "Cooldown"
    This reverts commit bcb47b15f4607b1b26a3c676b5de1f4b9c92f8c3.

--- Assets/Scripts/SeedPlanter.cs
+++ Assets/Scripts/SeedPlanter.cs
@@ -12,16 +12,6 @@ public class SeedPlanter : MonoBehaviour
     [SerializeField] private GameObject prefabPlant;
     [SerializeField] private KeyCode atalho;
     public static event Action OnPlantBought;
-<<<<<<< Updated upstream
-=======
-    [Header("Cooldown")]
-    [SerializeField] private float timerCooldownPlant;
-    [SerializeField] private float timerPlant;
-    [SerializeField] private bool buyableStart; //Se a planta pode ser comprada no início do jogo ou tem que esperar o cooldown 
-    [SerializeField] private Image imageCooldown;
-    [SerializeField] private bool compra; // Armazena o retorno do SunManager.BuyPlant()
-    [SerializeField] private bool origem = false; // Verifica a origem do evento de planta colocada
->>>>>>> Stashed changes
 
     void Start()
     {
@@ -37,25 +27,11 @@ public class SeedPlanter : MonoBehaviour
 
     [ContextMenu("Try Buy Plant")]
     public void TryBuyPlant() {
-<<<<<<< Updated upstream
         if (sunManager.BuyPlant(cost)) {
             Instantiate(prefabPlant, Input.mousePosition, Quaternion.identity);
             OnPlantBought?.Invoke();
             PlantPlacer.OnCancelBuy += CancelBuy;
             PlantPlacer.OnPlantPlaced += PlantPlaced;
-=======
-        compra = sunManager.BuyPlant(cost);
-        if (compra && timerCooldownPlant == 0) {
-            Instantiate(prefabPlant, Input.mousePosition, Quaternion.identity);
-            origem = true;
-            OnPlantBought?.Invoke();
-            PlantPlacer.OnCancelBuy += CancelBuy;
-            PlantPlacer.OnPlantPlaced += PlantPlaced;
-        }  
-        if(timerCooldownPlant > 0){
-            if (compra == false) {
-            } else sunManager.UndoBuy(cost);
->>>>>>> Stashed changes
         }
     }
 
@@ -66,14 +42,6 @@ public class SeedPlanter : MonoBehaviour
 
     private void PlantPlaced() {
         PlantPlacer.OnCancelBuy -= CancelBuy;
-<<<<<<< Updated upstream
         PlantPlacer.OnCancelBuy -= PlantPlaced;
-=======
-        PlantPlacer.OnCancelBuy -= PlantPlaced; 
-        if (origem) {
-            timerCooldownPlant = timerPlant;
-            origem = false;
-        }      
->>>>>>> Stashed changes
     }
 }
