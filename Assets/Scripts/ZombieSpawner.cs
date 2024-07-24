aab6ce776dd8de587231e17cfae6c543717bfefc
JoseSalsicha <netjj512@gmail.com>
2024-07-24 17:28:51 +0100

    ZSpawner

--- Assets/Scripts/ZombieSpawner.cs
+++ Assets/Scripts/ZombieSpawner.cs
@@ -1,29 +1,45 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class ZombieSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject zombie;
-    [SerializeField] private float timeBTZombies;
     [SerializeField] private float timer;
     [SerializeField] private float[] line;
+    [SerializeField] private float minTime;
+    [SerializeField] private float maxTime;
+    [SerializeField] private float timeBTZombies;
     
-    void Update()
+    void Awake()
     {
-        if (timer < timeBTZombies) {
-            timer += 1*Time.deltaTime;
-        } else {
+        RandomTimeSpawn();
+    }
+    void Update()
+    {    
+        timeBTZombies -= Time.deltaTime;
+        if (timeBTZombies < 0) {
             SpawnZombie();
-            }
+            RandomTimeSpawn();
+        }
+        timer += Time.deltaTime;
+        Wave();
     }
 
     public void SpawnZombie() {
         int index = UnityEngine.Random.Range(0, 5);
-        if (timer >= timeBTZombies) {
-            Instantiate(zombie, new Vector2 (transform.position.x, line[index]), quaternion.identity);
-            timer = 0;
+        Instantiate(zombie, new Vector2 (transform.position.x, line[index]), Quaternion.identity);
+        timeBTZombies -= Time.deltaTime;
+    }
+
+    public void RandomTimeSpawn(){
+        timeBTZombies = UnityEngine.Random.Range(minTime, maxTime);
+    }
+    public void Wave(){
+        if (timer > 100) {
+            timeBTZombies -= 2*Time.deltaTime;
+        } else if (timer > 50) {
+            timeBTZombies -= 1*Time.deltaTime;
         }
     }
 }
